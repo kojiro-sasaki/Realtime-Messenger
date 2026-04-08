952fd66112d917bbdac97d5780ffdb3cce15b4e5
kojiro-sasaki <makscac530@gmail.com>
2026-04-08 20:05:15 +0200

    add websocket server, closes #1

--- main.go
+++ main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server started on :8080")
-
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Server is running")
 
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Upgrager error")
+		return
+	}
+	defer conn.Close()
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("server started")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("server error :", err)
+	}
 }
