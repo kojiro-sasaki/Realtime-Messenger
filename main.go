4ed078aaf8d9474b880b9d81f20244adfc6b4f6e
kojiro-sasaki <makscac530@gmail.com>
2026-04-09 21:58:03 +0200

    fix websocket concurrency and improve locking

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -10,19 +11,22 @@ import (
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
-
+var mu sync.Mutex
 var clients = make(map[*websocket.Conn]bool)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("Upgrager error")
+		fmt.Println("Upgrager error", err)
 		return
 	}
+	mu.Lock()
 	clients[conn] = true
-
+	mu.Unlock()
 	defer func() {
+		mu.Lock()
 		delete(clients, conn)
+		mu.Unlock()
 		conn.Close()
 	}()
 	for {
@@ -30,16 +34,24 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			break
 		}
-		for client := range clients {
+		mu.Lock()
+		var conns []*websocket.Conn
+		for c := range clients {
+			conns = append(conns, c)
+		}
+		mu.Unlock()
+		for _, client := range conns {
 			err := client.WriteMessage(websocket.TextMessage, msg)
 			if err != nil {
+				mu.Lock()
 				client.Close()
 				delete(clients, client)
+				mu.Unlock()
 			}
 		}
+
 	}
 }
-
 func main() {
 	http.HandleFunc("/ws", wsHandler)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
