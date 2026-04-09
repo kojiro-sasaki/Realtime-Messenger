e87c380fa1c99ff0e0ef6613fea6accdc69f55a8
kojiro-sasaki <makscac530@gmail.com>
2026-04-09 21:45:58 +0200

    added broadcast and list of clients

--- main.go
+++ main.go
@@ -11,22 +11,31 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+var clients = make(map[*websocket.Conn]bool)
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Upgrager error")
 		return
 	}
+	clients[conn] = true
 
-	defer conn.Close()
+	defer func() {
+		delete(clients, conn)
+		conn.Close()
+	}()
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		err = conn.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			break
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
 		}
 	}
 }
