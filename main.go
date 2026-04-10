891534e3b33dc61ffac7a084ddae3181bfe74504
kojiro-sasaki <makscac530@gmail.com>
2026-04-10 16:43:21 +0200

    some updates

--- main.go
+++ main.go
@@ -12,7 +12,13 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 var mu sync.Mutex
-var clients = make(map[*websocket.Conn]bool)
+
+type Client struct {
+	conn *websocket.Conn
+	name string
+}
+
+var clients = make(map[*Client]bool)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -20,12 +26,21 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Upgrager error", err)
 		return
 	}
+	_, name, err := conn.ReadMessage()
+	if err != nil {
+		conn.Close()
+		return
+	}
+	client := &Client{
+		conn: conn,
+		name: string(name),
+	}
 	mu.Lock()
-	clients[conn] = true
+	clients[client] = true
 	mu.Unlock()
 	defer func() {
 		mu.Lock()
-		delete(clients, conn)
+		delete(clients, client)
 		mu.Unlock()
 		conn.Close()
 	}()
@@ -34,19 +49,24 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			break
 		}
+		fullMsg := []byte(client.name + ":" + string(msg))
+
 		mu.Lock()
-		var conns []*websocket.Conn
+		var conns []*Client
 		for c := range clients {
 			conns = append(conns, c)
 		}
 		mu.Unlock()
-		for _, client := range conns {
-			err := client.WriteMessage(websocket.TextMessage, msg)
+		for _, c := range conns {
+			if c == client {
+				continue
+			}
+			err := c.conn.WriteMessage(websocket.TextMessage, fullMsg)
 			if err != nil {
 				mu.Lock()
-				client.Close()
 				delete(clients, client)
 				mu.Unlock()
+				c.conn.Close()
 			}
 		}
 
