08c2f0bc15fc4fbb5f8411f3591820f5229eb054
kojiro-sasaki <makscac530@gmail.com>
2026-04-12 18:37:22 +0200

    update: switch to JSON-based messaging

--- hub.go
+++ hub.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"strings"
 	"sync"
 
@@ -10,6 +11,12 @@ import (
 var mu sync.Mutex
 
 var clients = make(map[*Client]bool)
+
+type Message struct {
+	Type    string `json:"type"`
+	Sender  string `json:"sender,omitempty"`
+	Message string `json:"message,omitempty"`
+}
 
 func broadcast(msg []byte) {
 	mu.Lock()
@@ -60,37 +67,68 @@ func removeClient(c *Client) {
 	mu.Unlock()
 	c.conn.Close()
 }
-
-func sendPrivateMessage(sender *Client, recipientName string, msg string) {
-	var target *Client
+func findClient(name string) *Client {
 	mu.Lock()
+	defer mu.Unlock()
+
 	for c := range clients {
-		if c.name == recipientName {
-			target = c
-			break
+		if c.name == name {
+			return c
 		}
 	}
-	mu.Unlock()
+	return nil
+}
+func sendPrivateMessage(sender *Client, recipient string, msg string) {
+	target := findClient(recipient)
+
 	if target == nil {
-		sendToClient(sender, []byte("[SYSTEM] User not found"))
+		sendJSON(sender, Message{
+			Type:    "system",
+			Message: "User not found",
+		})
 		return
 	}
-	sendToClient(target, []byte("[DM] "+sender.name+":"+msg))
-	sendToClient(sender, []byte("[DM to "+target.name+"] "+msg))
+
+	sendJSON(target, Message{
+		Type:    "dm",
+		Sender:  sender.name,
+		Message: msg,
+	})
+
+	sendJSON(sender, Message{
+		Type:    "dm",
+		Sender:  sender.name,
+		Message: "(to " + target.name + ") " + msg,
+	})
 }
 
-func handleCommand(c *Client, command string) bool {
-	if command == "/users" {
-		names := getUsernames()
-		sendToClient(c, []byte("[SYSTEM] Users: "+strings.Join(names, ", ")))
+func handleCommand(c *Client, text string) bool {
+
+	if text == "/help" {
+		sendJSON(c, Message{
+			Type:    "system",
+			Message: "/users, /msg <user> <msg>, /name <newname>, /help",
+		})
 		return true
 	}
 
-	if strings.HasPrefix(command, "/msg ") {
-		parts := strings.SplitN(command, " ", 3)
+	if text == "/users" {
+		names := getUsernames()
+		sendJSON(c, Message{
+			Type:    "system",
+			Message: "Users: " + strings.Join(names, ", "),
+		})
+		return true
+	}
+
+	if strings.HasPrefix(text, "/msg ") {
+		parts := strings.SplitN(text, " ", 3)
 
 		if len(parts) < 3 {
-			sendToClient(c, []byte("[SYSTEM] Usage: /msg <user> <message>"))
+			sendJSON(c, Message{
+				Type:    "system",
+				Message: "Usage: /msg <user> <message>",
+			})
 			return true
 		}
 
@@ -98,38 +136,65 @@ func handleCommand(c *Client, command string) bool {
 		return true
 	}
 
-	if strings.HasPrefix(command, "/name ") {
-		parts := strings.SplitN(command, " ", 2)
+	if strings.HasPrefix(text, "/name ") {
+		parts := strings.SplitN(text, " ", 2)
+
 		if len(parts) < 2 {
-			sendToClient(c, []byte("[SYSTEM] Usage: /name <newname>"))
+			sendJSON(c, Message{
+				Type:    "system",
+				Message: "Usage: /name <newname>",
+			})
 			return true
-
 		}
+
 		newname := strings.TrimSpace(parts[1])
+
 		if newname == "" {
-			sendToClient(c, []byte("[SYSTEM] Invalid name"))
+			sendJSON(c, Message{
+				Type:    "system",
+				Message: "Invalid name",
+			})
 			return true
-
 		}
+
 		if isNameTaken(newname) {
-			sendToClient(c, []byte("[SYSTEM]Name is already taken"))
+			sendJSON(c, Message{
+				Type:    "system",
+				Message: "Name already taken",
+			})
 			return true
-
 		}
-		oldname := c.name
-		c.name = newname
-		sendToClient(c, []byte("[SYSTEM] You change name to: "+newname))
-		broadcast([]byte("[SYSTEM]" + oldname + " changed to: " + newname))
-		return true
-	}
-	if command == "/help" {
-		sendToClient(c, []byte("[SYSTEM] Commands:\n"+
-			"/users - list of active users\n"+
-			"/msg <user> <msg> - private message\n"+
-			"/name <newname> - change name\n"+
-			"/help - show commands\n"))
-		return true
-	}
 
+		old := c.name
+		c.name = newname
+
+		sendJSON(c, Message{
+			Type:    "system",
+			Message: "Name changed to " + newname,
+		})
+
+		broadcastJSON(Message{
+			Type:    "system",
+			Message: old + " changed name to " + newname,
+		})
+
+		return true
+	}
 	return false
+}
+func broadcastJSON(v any) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return
+	}
+	broadcast(data)
+}
+func sendJSON(c *Client, v any) error {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.conn.WriteMessage(websocket.TextMessage, data)
 }
