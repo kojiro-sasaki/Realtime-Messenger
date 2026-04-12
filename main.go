4fce62b8cc0dd83d03172bc08dd242422b585bc6
kojiro-sasaki <makscac530@gmail.com>
2026-04-12 19:14:46 +0200

    Add : added graceful shutdown

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/ws", wsHandler)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	fmt.Println("server started")
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("server error :", err)
+	server := &http.Server{
+		Addr: ":8080",
 	}
+
+	go func() {
+		fmt.Println("server started")
+		if err := server.ListenAndServe(); err != nil {
+			fmt.Println("server error:", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+	fmt.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server.Shutdown(ctx)
+
 }
