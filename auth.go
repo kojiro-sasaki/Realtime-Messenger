83bccb578b8f8224d431895a0d4ed174ffcf2d06
kojiro-sasaki <makscac530@gmail.com>
2026-04-19 16:01:58 +0200

    Added: add brute force protection to login

--- auth.go
+++ auth.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -10,6 +12,56 @@ import (
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type loginAttempt struct {
+	count     int
+	blockedTo time.Time
+}
+
+type checkRequest struct {
+	ip       string
+	username string
+	resp     chan bool
+	success  bool
+}
+
+var loginChan = make(chan checkRequest, 100)
+
+func startLoginLimiter() {
+	attempts := make(map[string]*loginAttempt)
+
+	for req := range loginChan {
+		key := req.ip + ":" + req.username
+
+		att := attempts[key]
+		now := time.Now()
+
+		if att != nil && now.Before(att.blockedTo) {
+			req.resp <- false
+			continue
+		}
+
+		if req.success {
+			delete(attempts, key)
+			req.resp <- true
+			continue
+		}
+
+		if att == nil {
+			att = &loginAttempt{}
+			attempts[key] = att
+		}
+
+		att.count++
+
+		if att.count >= 5 {
+			att.blockedTo = now.Add(1 * time.Minute)
+			att.count = 0
+		}
+
+		req.resp <- true
+	}
 }
 
 func hashPass(password string) (string, error) {
@@ -85,23 +137,54 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var hash string
+		ip := strings.Split(r.RemoteAddr, ":")[0]
 
+		resp := make(chan bool)
+		loginChan <- checkRequest{
+			ip:       ip,
+			username: u.Username,
+			resp:     resp,
+			success:  false,
+		}
+
+		if !<-resp {
+			http.Error(w, "Too many attempts. Try later.", 429)
+			return
+		}
+		var hash string
 		err := db.QueryRow(
 			"SELECT password FROM users WHERE username=?",
 			u.Username,
 		).Scan(&hash)
 
 		if err != nil || !checkPass(hash, u.Password) {
+
+			resp := make(chan bool)
+			loginChan <- checkRequest{
+				ip:       ip,
+				username: u.Username,
+				resp:     resp,
+				success:  false,
+			}
+			<-resp
+
 			http.Error(w, "invalid credentials", http.StatusUnauthorized)
 			return
 		}
+		resp = make(chan bool)
+		loginChan <- checkRequest{
+			ip:       ip,
+			username: u.Username,
+			resp:     resp,
+			success:  true,
+		}
+		<-resp
 
 		http.SetCookie(w, &http.Cookie{
 			Name:     "user",
 			Value:    u.Username,
 			HttpOnly: true,
-			Secure:   true,
+			Secure:   false,
 			SameSite: http.SameSiteStrictMode,
 			Path:     "/",
 		})
